029ab2bcf5df8178db8393a016fbec3ae0452128
Marty Mazurik <marty@martymazurik.com>
2019-02-22 16:01:09 -0700

    simple initial example for GET REST API call

---
+++ src/restapi/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// our main function
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/people", GetPeople).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+// Person : is for person
+type Person struct {
+	ID        string   `json:"id,omitempty"`
+	Firstname string   `json:"firstname,omitempty"`
+	Lastname  string   `json:"lastname,omitempty"`
+	Address   *Address `json:"address,omitemplty"`
+}
+
+// Address : is for addresses
+type Address struct {
+	City  string `json:"city,omitempty"`
+	State string `json:"state,omitempty"`
+}
+
+var people []Person
+
+// GetPeople : gets people
+func GetPeople(w http.ResponseWriter, r *http.Request) {
+	people = append(people, Person{ID: "1", Firstname: "Marty", Lastname: "Mazurik", Address: &Address{City: "Happy Valley", State: "OR"}})
+	json.NewEncoder(w).Encode(people)
+}
