321b1f540078ad909d5f47796bab5ed0eccb4407
Marty Mazurik <marty@martymazurik.com>
2019-03-01 12:54:20 -0700

    update helloworld

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -6,10 +6,6 @@ import (
 )
 
 func main() {
-<<<<<<< HEAD
-	fmt.Printf("Hello, World\n")
-}
-=======
 	fmt.Println("Hello, World times", addTwo(2, 3)) // initial cap;  expose Println method "outside of the package", implies l.c. would be internal-only
 	fmt.Println("Go O.S. : ", runtime.GOOS)
 }
@@ -17,4 +13,3 @@ func main() {
 func addTwo(a int, b int) int {
 	return a + b
 }
->>>>>>> updated helloworld
