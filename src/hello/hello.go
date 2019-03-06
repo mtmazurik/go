e6c2baab2befceb26e003c6a9248d5f790a1ef26
Marty Mazurik <marty@martymazurik.com>
2019-03-05 17:29:43 -0700

    update functions

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -5,15 +5,19 @@ import (
 	"os"
 	"runtime"
 	"strconv"
+	"strings"
 )
 
-func main() {
+func main() { // no input parms, no return values, called automatically
 	myUsername := os.Getenv("USERNAME") // os pkg; get Windows env var
+	theOS := runtime.GOOS
 
 	fmt.Println("Hello, World times", addTwo(2, 3)) // methods have init cap; Println auto-converts int result
-	fmt.Println("Go O.S. : ", runtime.GOOS)         // runtime pkg; check the GO Operating System (GOOS)
+	fmt.Println("Go O.S. : ", theOS)                // runtime pkg; check the GO Operating System (GOOS)
 	fmt.Println("Thank you. My name is", myUsername)
 	fmt.Println(playWithString(" the number is: ", 5))
+	fmt.Println(converter(theOS, myUsername))
+	fmt.Println("Top score: ", getBestScore(13, 10, 13, 17, 14, 16))
 }
 
 func addTwo(a int, b int) int {
@@ -22,4 +26,22 @@ func addTwo(a int, b int) int {
 
 func playWithString(theString string, theNumber int) string { // post notation of type, string is return type
 	return theString + strconv.Itoa(theNumber) // strconv package: integer to ASCII
+}
+
+func converter(os, username string) (retOS, retUsername string) { // multiple return values
+	convertedOS := strings.Title(os)
+	username = strings.ToUpper(username)
+	return convertedOS, username
+}
+
+func getBestScore(finishes ...int) int { // variadic function (don't know number of variables coming in) uses elipsis ...
+
+	best := finishes[0] // 1st item in the list
+
+	for _, i := range finishes {
+		if i > best {
+			best = i
+		}
+	}
+	return best
 }
