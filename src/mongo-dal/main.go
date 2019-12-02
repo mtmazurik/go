34d78e758c03ff6498c5fc1abd8b92bccaa364dc
Marty Mazurik <marty@martymazurik.com>
2019-12-01 17:19:51 -0800

    interim check-in

--- src/mongo-dal/main.go
+++ src/mongo-dal/main.go
@@ -1,15 +1,5 @@
 package main
 
-import (
-	"context"
-    "fmt"
-    "log"
-
-    "go.mongodb.org/mongo-driver/bson"
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
-)
-
 type Trainer struct {
 	Name string
 	Age  int
